6eb442b91fde757a8c6314cb38696c890af96a1f
willdyoungs <willdyoungs@gmail.com>
2016-08-05 15:57:18 +0000

    Removed resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :pieces
+  # resources :pieces
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pieces#home'
   get '/' => 'pieces#home'
